196ced044ea322a851502113f1afb90b7f1c5d64
= <ramirocanario1@gmail.com>
2021-10-23 01:57:25 -0300

    en funcionamiento

--- curva-de-hilbert.py
+++ curva-de-hilbert.py
@@ -31,7 +31,7 @@ shapes = [
     'classic'
 ]
 
-cadena_inicial = 'X'    # TODO puede ser parametro?
+# cadena_inicial = 'X'    # TODO puede ser parametro?
 reglas = {
     'X':'+YF-XFX-FY+',
     'Y':'-XF+YFY+FX-'
@@ -50,6 +50,15 @@ def procesarCadena(cadena):
 
     return resultado
 
+def dibujar(cadena, tortuga, segmento, angulo):
+    for simbolo in cadena:
+        if simbolo == 'F':
+            tortuga.forward(segmento)
+        elif simbolo == '+':
+            tortuga.left(angulo)
+        elif simbolo == '-':
+            tortuga.right(angulo)
+
 @click.command()
 @click.option('-i', '--iteraciones', required=True, type=click.IntRange(1, 10, clamp=True), help='Cantidad de iteraciones (1..10).')
 @click.option('-s', '--segmento', default=10, show_default=True, type=int, help='Longitud en px de cada segmento.')
@@ -60,9 +69,11 @@ def procesarCadena(cadena):
 @click.option('-w', '--width', default=2, show_default=True, type=int, help='Grosor de la linea (px).')
 @click.option('--ocultar/--no-ocultar', default=False, show_default=True, help='Ocultar o mostrar cursor.')
 @click.option('-sh', '--shape', default='arrow', show_default=True, type=click.Choice(shapes), help='Forma del cursor.')
+@click.option('-t', '--tracer', default=1, show_default=True, type=click.IntRange(1, 5, clamp=True), help='Periodo de actualizacion de pantalla')
+@click.option('-ci', '--cadena-inicial', default='X', show_default=True, help='Cadena inicial')
 @click.option('-c', '--mostrar-cadena', default=False, show_default=True, type=bool, help='Mostrar cadena de simbolos.')
 
-def programa(iteraciones, segmento, angulo, velocidad, background, pencolor, width, ocultar, shape, mostrar_cadena):
+def programa(iteraciones, segmento, angulo, velocidad, background, pencolor, width, ocultar, shape, tracer, cadena_inicial, mostrar_cadena):
     print('''Parametros de ejecucion: 
     -> Cantidad de iteraciones: {}
     -> Longitud de segmento: {} px
@@ -73,8 +84,10 @@ def programa(iteraciones, segmento, angulo, velocidad, background, pencolor, wid
     -> Ancho de linea: {} px
     -> Mostrar cursor: {}
     -> Forma de cursor: {}
+    -> Periodo de actualizacion: {}
+    -> Cadena inicial: {}
     -> Mostrar cadena: {}  '''
-    .format(iteraciones, segmento, angulo, velocidad, background, pencolor, width, ocultar, shape, mostrar_cadena))
+    .format(iteraciones, segmento, angulo, velocidad, background, pencolor, width, ocultar, shape, tracer,cadena_inicial, mostrar_cadena))
 
     cadena = cadena_inicial
 
@@ -82,10 +95,34 @@ def programa(iteraciones, segmento, angulo, velocidad, background, pencolor, wid
         cadena = procesarCadena(cadena)
 
     if mostrar_cadena:
-        input('\nPresione una tecla para mostrar la cadena')
+        input('\nPresione una tecla para mostrar la cadena.')
         print(cadena)
+    else:
+        input('\nPresione una tecla para comenzar a dibujar.')
 
-    
+    # Creo una ventana y una tortuga
+    ventana = turtle.Screen()
+    ventana.title('Curva de Hilbert')
+    ventana.setup(width=.8, height=.8)  # TODO parametros?
+    tortuga = turtle.Turtle()
+
+    # Posiciono la tortuga
+    tortuga.penup()
+    tortuga.setposition(-400, -400) #TODO hardcodeado!
+    tortuga.pendown()
+
+    # Configuro el dibujo a partir de los parametros
+    tortuga.speed(velocidad)
+    ventana.bgcolor(background)
+    tortuga.pencolor(pencolor)
+    tortuga.width(width)
+    if ocultar:
+        tortuga.hideturtle()
+    tortuga.shape(shape)
+    ventana.tracer(tracer)
+
+    dibujar(cadena, tortuga, segmento, angulo)
+    turtle.done()
 
 if __name__ == '__main__':
     programa()
